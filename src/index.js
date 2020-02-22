854d37457a4c7751864e995cf4712212df37cdea
Enrique Devars <e.devars.dev@gmail.com>
2020-02-21 18:20:51 -0600

    :sparkles: linting and now deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>React config</h1>, document.getElementById("app"));
+ReactDOM.render(<h1>React configuration</h1>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   output: {
-    filename: "app.bundle.js"
+    filename: 'app.bundle.js'
   },
   module: {
     rules: [
@@ -10,9 +10,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
@@ -20,7 +20,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/public/index.html"
+      template: 'src/public/index.html'
     })
   ]
-};
+}
