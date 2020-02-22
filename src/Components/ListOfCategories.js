629449dff5618394ac302cbaa026d6cc2b23ee91
Enrique Devars <e.devars.dev@gmail.com>
2020-02-21 21:31:44 -0600

    :sparkles: Connecting API to list of categories

--- src/Components/ListOfCategories.js
+++ src/Components/ListOfCategories.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Category } from './Category'
-import { categories } from '../../api/db.json'
 
 const List = styled.ul`
   display: flex;
@@ -16,12 +15,28 @@ const Item = styled.li`
   padding: 0 8px;
 `
 
-export const ListOfCategories = () => (
-  <List>
-    {categories.map(category => (
-      <Item key={category.id}>
-        <Category {...category} />
-      </Item>
-    ))}
-  </List>
-)
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    async function fetchData () {
+      const response = await window.fetch(
+        'https://petgram-server-alpha.now.sh/categories'
+      )
+      const data = await response.json()
+      setCategories(data)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <List>
+      {categories.map(category => (
+        <Item key={category.id}>
+          <Category {...category} />
+        </Item>
+      ))}
+    </List>
+  )
+}
