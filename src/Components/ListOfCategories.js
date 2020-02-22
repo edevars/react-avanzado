7693a7d90fa644503d4667d5d484d97e6fc59862
Enrique Devars <e.devars.dev@gmail.com>
2020-02-22 11:10:35 -0600

    :sparkles: Adding nprogress bar loading

--- src/Components/ListOfCategories.js
+++ src/Components/ListOfCategories.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled, { css } from 'styled-components'
+import NProgress from 'nprogress'
 import { Category } from './Category'
 import { bounceDown } from '../styles/animation'
 
@@ -35,10 +36,12 @@ const getCategoriesData = () => {
 
   useEffect(() => {
     async function fetchData () {
+      NProgress.start()
       const response = await window.fetch(
         'https://petgram-server-alpha.now.sh/categories'
       )
       const data = await response.json()
+      NProgress.done()
       setCategories(data)
     }
 
