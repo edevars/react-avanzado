bcabbab3bec8ec5d2a136f11f84620664c9bdbcf
Enrique Devars <e.devars.dev@gmail.com>
2020-02-21 20:15:57 -0600

    :sparkles: Adding logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { GlobalStyle } from './GlobalStyle'
+import { Logo } from './Components/Logo'
 import { ListOfCategories } from './Components/ListOfCategories'
 import { ListOfPhotoCards } from './Components/ListOfPhotoCards'
 
 export const App = () => (
   <>
+    <Logo />
     <GlobalStyle />
     <ListOfCategories />
     <ListOfPhotoCards />
