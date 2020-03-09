e0eae47dead09e0918dccbb4f8495217c21c2586
Enrique Devars <e.devars.dev@gmail.com>
2020-03-09 14:24:58 -0600

    :sparkles: Adding routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Logo } from './Components/General/Logo'
-import { ListOfCategories } from './Components/ListOfCategories'
-import { ListOfPhotoCards } from './Containers/ListOfPhotoCards'
+import { Home } from './pages/home'
 import { PhotoCardWithQuery } from './Containers/PhotoCardWithQuery'
 
 export const App = () => {
@@ -17,10 +17,10 @@ export const App = () => {
       {detailId ? (
         <PhotoCardWithQuery id={detailId} />
       ) : (
-        <>
-          <ListOfCategories />
-          <ListOfPhotoCards categoryId={1} />
-        </>
+        <Router>
+          <Home path='/' />
+          <Home path='/pet/:id' />
+        </Router>
       )}
     </>
   )
