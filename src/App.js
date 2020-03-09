b1950f64b68ddc7a3117855ce7156ab16d69157f
Enrique Devars <e.devars.dev@gmail.com>
2020-03-09 15:09:40 -0600

    :sparkles: Detail view

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Logo } from './Components/General/Logo'
 import { Home } from './pages/home'
-import { PhotoCardWithQuery } from './Containers/PhotoCardWithQuery'
+import { Detail } from './pages/Detail'
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-  console.log(detailId)
-
   return (
     <>
       <GlobalStyle />
       <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery id={detailId} />
-      ) : (
-        <Router>
-          <Home path='/' />
-          <Home path='/pet/:id' />
-        </Router>
-      )}
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </>
   )
 }
---
+++ src/pages/Detail.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { PhotoCardWithQuery } from '../Containers/PhotoCardWithQuery'
+
+export const Detail = ({ detailId }) => {
+  return <PhotoCardWithQuery id={detailId} />
+}
