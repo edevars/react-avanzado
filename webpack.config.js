d42e05f24f2d2ab6779ad587d2237b8013f6e249
Enrique Devars <e.devars.dev@gmail.com>
2020-03-16 16:24:18 -0600

    :sparkles: Progressive Web App

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -23,6 +26,41 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/public/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram | Tu app de fotos',
+      shortname: 'Petgram 🐶',
+      description: 'Encuentra fotos de animales domesticos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.clouginary.com)|(images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp(
+            'https://petgram-server-alpha.now.sh'
+          ),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'API'
+          }
+        }
+      ]
     })
   ]
 }
